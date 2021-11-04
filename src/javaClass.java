2d2bd8463d3269c49772a4285366752292ae330b
faruk <faruk373@gmail.com>
2021-11-04 20:35:56 +0100

    Initial commit

---
+++ src/javaClass.java
@@ -0,0 +1,27 @@
+public class javaClass {
+    public static void main(String[] args) {
+        int number = 5;
+        double doubly = 4.6;
+        char firstInitial = 'f';
+        String name = "faruk";
+        boolean isHouse = true;
+
+       /**System.out.println(number * 5);
+        System.out.println(doubly);
+        System.out.println(firstInitial);
+        System.out.println(name);
+        System.out.println(isHouse);*/
+
+       if(name.equals("faruk")) {
+           System.out.println("birth day boy");
+
+       } else {
+           System.out.println("hello");
+       }
+
+
+
+
+
+    }
+}
